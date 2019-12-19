04a0100a5bcd282b4f9b409dcb330fbf192e4e2a
kusumajati <nino.bmakj@outlook.com>
2019-12-20 01:48:36 +0700

    fix router link to

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,11 +1,11 @@
 <template>
   <div class="about">
     <nav class="nav">
-      <router-link to="/login" tag="a" class="nav-link">Login</router-link>
-      <router-link to="/user" tag="a" class="nav-link">All User</router-link>
-  <router-link to="/" tag='a' class="nav-link"> Home </router-link>
-  <router-link to="/vuex" tag="a" class="nav-link">vuex</router-link>
-  <router-link to="/computed" class="nav-link" tag="a">Computed</router-link>
+      <router-link to="/login" tag="a" class="nav-link" exact>Login</router-link>
+      <router-link to="/user" tag="a" class="nav-link" exact>All User</router-link>
+  <router-link to="/" tag='a' class="nav-link" exact> Home </router-link>
+  <router-link to="/vuex" tag="a" class="nav-link" exact>vuex</router-link>
+  <router-link to="/computed" class="nav-link" tag="a" exact>Computed</router-link>
   </nav>
   </div>
 </template>
